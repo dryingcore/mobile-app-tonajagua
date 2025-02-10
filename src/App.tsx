46e17adbb04a0d48130ab316a2fb4a128d546d91
Gabriel Rocha <protonmail.default.sense07@protonmail.com>
2025-02-10 13:22:05 -0300

    modified:   src/App.tsx 	modified:   src/components/Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "@mui/material";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -9,11 +10,13 @@ function App() {
       <Container
         sx={{
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           height: "90vh",
         }}
       >
+        <Header />
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
