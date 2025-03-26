520b191fedf0c33cb94ffdf53d1c797ab49f6cda
Gabriel Rocha <protonmail.default.sense07@protonmail.com>
2025-03-26 13:58:53 +0000

    Mar 26, 2025, 10:58 AM

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -51,7 +51,6 @@ const CategoryList: React.FC<CategoryListProps> = ({ onSelectCategory }) => {
     <Box sx={{ width: "100%", padding: "10px" }}>
       <Typography variant="h6" fontWeight="bold" sx={{ mb: 1, mt: 2 }}>
         Categorias
- 
       </Typography>
 
       <Box
