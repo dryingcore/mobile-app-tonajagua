2af012ff9cc69099fe9ac43a47b9f4bd029a86cc
Gabriel Rocha <protonmail.default.sense07@protonmail.com>
2025-03-26 12:24:17 +0000

    Mar 26, 2025, 9:24 AM

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import ItemList from "../components/ItemList";
 import CategoryList from "../components/CategoryList";
 import ImageCarrossel from "../components/ImageCarrossel";
 
 export default function HomePage() {
+  const [selectedCategory, setSelectedCategory] = useState<
+    string | undefined
+  >();
+
   return (
     <>
       <Header />
       <ImageCarrossel />
-      <CategoryList />
+      <CategoryList onSelectCategory={setSelectedCategory} />
       <hr style={{ width: "100%", height: "0px" }} />
-      <ItemList />
+      <ItemList categoria={selectedCategory} />
     </>
   );
 }
