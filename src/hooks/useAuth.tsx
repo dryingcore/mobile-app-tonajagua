26decb966649f4b63acd901338e03cc1bdf45f6e
Gabriel Rocha <protonmail.default.sense07@protonmail.com>
2025-03-26 13:44:31 +0000

    Mar 26, 2025, 10:44 AM

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import Auth from "../context/AuthContext";
+import AuthContext from "../context/AuthContext";
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
