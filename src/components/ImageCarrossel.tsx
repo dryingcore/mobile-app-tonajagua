ca031abda9d604c7b350937425b70702d2ef8527
Gabriel Rocha <protonmail.default.sense07@protonmail.com>
2025-03-31 01:06:22 +0000

    Mar 30, 2025, 10:06 PM

--- src/components/ImageCarrossel.tsx
+++ src/components/ImageCarrossel.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useState, useEffect } from "react";
+import { ReactElement, useState } from "react";
 import { Box } from "@mui/material";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -15,34 +15,12 @@ const images = [
 
 export default function ImageCarrossel() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [storedImages, setStoredImages] = useState<string[]>([]);
 
-  useEffect(() => {
-    // Verificar se as imagens estão no localStorage
-    const storedData = localStorage.getItem("images");
-
-    if (storedData) {
-      // Se as imagens estão no localStorage, use-as
-      setStoredImages(JSON.parse(storedData));
-    } else {
-      // Caso contrário, faça o processo de armazenar as imagens
-      const imageUrls = images.map((img) => `${imageBaseUrl}${img}`);
-      setStoredImages(imageUrls);
-
-      // Salve no localStorage
-      localStorage.setItem("images", JSON.stringify(imageUrls));
-    }
-  }, []);
-
-  const imagesToShow: ReactElement[] = storedImages.map((imgUrl, index) => {
-    const altText = images[index]
-      .replace(/-/g, " ")
-      .replace(".jpg", "")
-      .toUpperCase();
-
+  const imagesToShow: ReactElement[] = images.map((img) => {
+    console.log(`${imageBaseUrl}${img}`);
     return (
       <Box
-        key={imgUrl}
+        key={img}
         sx={{
           width: "100%",
           height: "100px",
@@ -53,8 +31,8 @@ export default function ImageCarrossel() {
         }}
       >
         <img
-          src={imgUrl}
-          alt={altText}
+          src={`${imageBaseUrl}${img}`}
+          alt={img}
           style={{
             width: "100%",
             height: "100%",
