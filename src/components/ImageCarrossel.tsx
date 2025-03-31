33a5ee94fce5ebb6d71b2d536cb2d52b08028d58
Gabriel Rocha <protonmail.default.sense07@protonmail.com>
2025-03-31 01:10:44 +0000

    Mar 30, 2025, 10:10 PM

--- src/components/ImageCarrossel.tsx
+++ src/components/ImageCarrossel.tsx
@@ -1,7 +1,6 @@
-import { ReactElement, useState, useEffect } from "react";
+import { ReactElement, useState } from "react";
 import { Box } from "@mui/material";
 import { Carousel } from "react-responsive-carousel";
-import { Filesystem, Directory, Encoding } from "@capacitor/filesystem";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const imageBaseUrl = "https://s01.decodesoftware.tech/uploads/camacho/";
@@ -16,88 +15,12 @@ const images = [
 
 export default function ImageCarrossel() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [storedImages, setStoredImages] = useState<string[]>([]);
 
-  // Função para baixar e salvar a imagem no dispositivo
-  const downloadAndStoreImage = async (imageUrl: string, fileName: string) => {
-    try {
-      const response = await fetch(imageUrl);
-      const blob = await response.blob();
-      const base64 = await convertBlobToBase64(blob);
-
-      // Salva o arquivo no sistema de arquivos do dispositivo
-      await Filesystem.writeFile({
-        path: `camacho_images/${fileName}`,
-        data: base64 as string,
-        directory: Directory.Data,
-        encoding: Encoding.UTF8,
-      });
-
-      return `file:///data/data/tech.decodesoftware.app/files/camacho_images/${fileName}`; // Caminho do arquivo salvo
-    } catch (error) {
-      console.error("Erro ao salvar a imagem:", error);
-      return null;
-    }
-  };
-
-  // Converter Blob para Base64
-  const convertBlobToBase64 = (blob: Blob) => {
-    return new Promise<string>((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        resolve(reader.result as string);
-      };
-      reader.onerror = reject;
-      reader.readAsDataURL(blob);
-    });
-  };
-
-  useEffect(() => {
-    const loadImages = async () => {
-      // Verificar se as imagens estão armazenadas no dispositivo
-      const storedData = localStorage.getItem("images");
-
-      if (storedData) {
-        // Se as imagens estão armazenadas, use-as
-        setStoredImages(JSON.parse(storedData));
-      } else {
-        // Caso contrário, baixe as imagens e salve no dispositivo
-        const imageUrls = await Promise.all(
-          images.map(async (img) => {
-            const fileName = img;
-            const imageUrl = `${imageBaseUrl}${img}`;
-            const localImagePath = await downloadAndStoreImage(
-              imageUrl,
-              fileName
-            );
-
-            return localImagePath;
-          })
-        );
-
-        // Filtra qualquer valor null
-        const validImageUrls = imageUrls.filter(
-          (url) => url !== null
-        ) as string[];
-
-        // Salve o caminho das imagens no localStorage
-        setStoredImages(validImageUrls);
-        localStorage.setItem("images", JSON.stringify(validImageUrls));
-      }
-    };
-
-    loadImages();
-  }, []);
-
-  const imagesToShow: ReactElement[] = storedImages.map((imgUrl, index) => {
-    const altText = images[index]
-      .replace(/-/g, " ")
-      .replace(".jpg", "")
-      .toUpperCase();
-
+  const imagesToShow: ReactElement[] = images.map((img) => {
+    console.log(`${imageBaseUrl}${img}`);
     return (
       <Box
-        key={imgUrl}
+        key={img}
         sx={{
           width: "100%",
           height: "100px",
@@ -108,8 +31,8 @@ export default function ImageCarrossel() {
         }}
       >
         <img
-          src={imgUrl} // Agora usa o caminho local da imagem
-          alt={altText}
+          src={`${imageBaseUrl}${img}`}
+          alt={img}
           style={{
             width: "100%",
             height: "100%",
